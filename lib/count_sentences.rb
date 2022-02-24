fa29a85d7fe57a69b0a854c82e78167541b32140
yeohoon yoon <yhoon.yoon@gmail.com>
2022-02-24 18:54:54 -0500

    completed

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,41 @@
 require 'pry'
 
 class String
+  attr_accessor :sentence, :question, :exclamation, :count
 
-  def sentence?
-    
+  def sentence?(word)
+    self.sentence = word.end_with?(".")
   end
 
-  def question?
-
+  def question? (que)
+    self.question = que.end_with?("?")
   end
 
-  def exclamation?
-
+  def exclamation? (exc)
+    self.exclamation = exc.end_with?("!")
   end
 
-  def count_sentences
-
+  def count_sentences (words)
+    self.count = words.split(" ").count
   end
 end
+
+word4=String.new
+word4.count_sentences("This is a string! It has three sentences. Right?")
+word4.exclamation?("This is a string! It has three sentences. Right?")
+puts word4.exclamation
+puts word4.count
+
+
+word3=String.new
+word3.exclamation?("asdklfjhaskldfhjkdas?")
+puts word3.exclamation
+
+
+word1=String.new
+word1.sentence?("asdfasdf")
+puts word1.sentence
+
+word2=String.new
+word2.question?("asdfasdf?")
+puts word2.question
